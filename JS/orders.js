52fcb1ef0dcefd2cf843155000af3cd1517a9812
Igor Silva <116300716+igorrsilvaaf@users.noreply.github.com>
2023-03-05 13:00:41 -0300

    Projeto de dashboard

---
+++ JS/orders.js
@@ -0,0 +1,36 @@
+const Orders = [
+	{
+		productName: 'Teclado Epomaker',
+		productNumber: '1234',
+		paymentStatus: 'Cartão de crédito',
+		shipping: 'Enviado!'
+	},
+
+	{
+		productName: 'Mouse hyperX viper',
+		productNumber: '9992',
+		paymentStatus: 'Cartão de débito',
+		shipping: 'Pendente'
+	},
+
+	{
+		productName: 'Monitor Acer',
+		productNumber: '09285',
+		paymentStatus: 'Boleto Bancario',
+		shipping: 'Analisando...'
+	},
+
+	{
+		productName: 'Cadeira gamer',
+		productNumber: '1234512345',
+		paymentStatus: 'Cartão de crédito',
+		shipping: 'Cancelado'
+	},
+
+	{
+		productName: 'Luminaria de monitor',
+		productNumber: '959594',
+		paymentStatus: 'Boleto Bancario',
+		shipping: 'Pagamento Rejeitado'
+	}
+]
